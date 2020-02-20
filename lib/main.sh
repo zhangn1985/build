7de03f48af3035b584bebaacba6e091fe610eec4
Zhang Ning <832666+zhangn1985@users.noreply.github.com>
2020-02-20 14:33:05 +0800

    do not create desktop package
    Signed-off-by: Zhang Ning <832666+zhangn1985@users.noreply.github.com>

--- lib/main.sh
+++ lib/main.sh
@@ -464,7 +464,7 @@ UBOOT_VER=$(dpkg --info "${DEB_STORAGE}/${CHOSEN_UBOOT}_${UBOOT_VERSION}+${SUBRE
 [[ -n $RELEASE && ! -f ${DEB_STORAGE}/$RELEASE/${CHOSEN_ROOTFS}_${REVISION}_${ARCH}.deb ]] && create_board_package
 
 # create desktop package
-[[ -n $RELEASE && ! -f ${DEB_STORAGE}/$RELEASE/${CHOSEN_DESKTOP}_${REVISION}_all.deb ]] && create_desktop_package
+#[[ -n $RELEASE && ! -f ${DEB_STORAGE}/$RELEASE/${CHOSEN_DESKTOP}_${REVISION}_all.deb ]] && create_desktop_package
 
 # build additional packages
 [[ $EXTERNAL_NEW == compile ]] && chroot_build_packages
